35308650bf58b2e98d12555b25b0fe79f8b06e8d
stefaniewangphd <stefaniegwang@gmail.com>
2023-01-17 13:59:15 -0600

    create operate function and update display with clicked keys

--- script.js
+++ script.js
@@ -14,12 +14,34 @@ const divide = function(a,b) {
     return a/b;
 }
 
-
-const operate = function(a,b) {
-    let operation=getOperation();
-    console.log(operation);
-    return operation(a,b)
+const operate = function(operation, x,y) {
+    const a = Number(x);
+    const b = Number(y);
+    switch(operation) {
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case 'X':
+            return multiply(a,b);
+        case '÷':
+            if (b===0) {
+                return 'undefined'
+            } else {
+                return divide(a,b)
+            }
+    }
 }
 
 const clearBtn = document.querySelector('#clear');
 clearBtn.addEventListener('click',() => location.reload());
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => { button.addEventListener('click', function() {
+    // console.log(this.id);
+    input.textContent=this.id;
+}) });
+
+const display=document.querySelector('#input');
+const input=document.createElement('div');
+display.appendChild(input);
